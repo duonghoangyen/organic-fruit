2dc7b889426153038d751c77caa9c82fc26ddf71
duonghoangyen <haiiyenn210@gmail.com>
2022-06-18 19:59:45 +0700

    yen ok

--- public/QLNS.sql
+++ public/QLNS.sql
@@ -1,6 +1,6 @@
-Create database QLNS;
+Create database QLNS
 go
-use QLNS;
+use QLNS
 go
 Create table Banner(
 ID int identity primary key,
@@ -8,9 +8,9 @@ Code nvarchar(50),
 Name nvarchar(150),
 Images nvarchar(150),
 Status bit,
-CreatedDate date,
-ModiredDate date
-);
+CreatedDate date DEFAULT GETDATE(),
+ModiredDate date DEFAULT GETDATE()
+)
 go
 Create table Blog(
 ID int identity primary key,
@@ -19,9 +19,9 @@ Name nvarchar(150),
 Description nvarchar(200),
 Images nvarchar(150),
 Status bit,
-CreatedDate date,
-ModiredDate date
-);
+CreatedDate date DEFAULT GETDATE(),
+ModiredDate date DEFAULT GETDATE()
+)
 go
 Create table Account(
 ID int identity primary key,
@@ -31,11 +31,11 @@ Email nvarchar(150),
 Phone nvarchar(150),
 Address nvarchar(150),
 Birthday nvarchar(150),
-Role bit,
 Gender bit,
-CreatedDate date,
-ModiredDate date
-);
+Role bit,
+CreatedDate date DEFAULT GETDATE(),
+ModiredDate date DEFAULT GETDATE()
+)
 go
 Create table Category(
 ID int identity primary key,
@@ -43,9 +43,9 @@ Code nvarchar(50),
 Name nvarchar(150),
 Description nvarchar(200),
 Status bit,
-CreatedDate date,
-ModiredDate date
-);
+CreatedDate date DEFAULT GETDATE(),
+ModiredDate date DEFAULT GETDATE()
+)
 go
 Create table Product(
 ID int identity primary key,
@@ -54,45 +54,43 @@ Name nvarchar(150),
 CategoryID int foreign key references Category(ID),
 Description nvarchar(200),
 Status bit,
-CreatedDate date,
-ModiredDate date
-);
+CreatedDate date DEFAULT GETDATE(),
+ModiredDate date DEFAULT GETDATE()
+)
 go
-Create table Order(
+Create table Orders(
 ID int identity primary key,
 Code nvarchar(50),
 Name nvarchar(150),
-Email nvarchar(150),
-Phone nvarchar(150),
+Email varchar(150),
+Phone varchar(150),
 Address nvarchar(150),
-TotalQuantity float,
+TotalQuantity int,
 TotalPrice float,
 Status bit,
 AccountID int foreign key references Account(ID),
-CreatedDate date,
-ModiredDate date
-);
+CreatedDate date DEFAULT GETDATE(),
+ModiredDate date DEFAULT GETDATE()
+)
 
 go
 Create table OrderDetail(
 ID int identity primary key,
 ProductID int foreign key references Product(ID),
-OrderID int foreign key references Order(ID),
+OrderID int foreign key references Orders(ID),
 Price float,
 Quantity int,
-CreatedDate date,
-ModiredDate date
-);
+CreatedDate date DEFAULT GETDATE(),
+ModiredDate date DEFAULT GETDATE()
+)
 go
-Create table OrderDetail(
+Create table Favorite(
 ID int identity primary key,
 ProductID int foreign key references Product(ID),
 AccountID int foreign key references Account(ID),
-Price float,
-Quantity int,
-CreatedDate date,
-ModiredDate date
-);
+CreatedDate date DEFAULT GETDATE(),
+ModiredDate date DEFAULT GETDATE()
+)
 go
 Create table Rating(
 ID int identity primary key,
@@ -101,6 +99,6 @@ AccountID int foreign key references Account(ID),
 Star float,
 Description nvarchar(200),
 Status bit,
-CreatedDate date,
-ModiredDate date
-);
+CreatedDate date DEFAULT GETDATE(),
+ModiredDate date DEFAULT GETDATE()
+)
